26bdf58781f0b0437f6bbeabeff857ebf3734be3
Archimedes <archimedessssssss@gmail.com>
2024-07-29 10:43:20 +1000

    update MusicScreenModel

---
+++ DialogDroid/Presentation/MusicSettingsScreen/Model/MusicScreenModel.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+enum MusicCollection: Int, CaseIterable {
+    case music1
+    case music2
+    case music3
+    case music4
+    case music5
+    case music6
+    case music7
+
+    var title: String {
+        switch self {
+        case .music1:
+            R.string.localizable.musicCollectionItemSoft()
+        case .music2:
+            R.string.localizable.musicCollectionItemRock()
+        case .music3:
+            R.string.localizable.musicCollectionItemElectro()
+        case .music4:
+            R.string.localizable.musicCollectionItemTechno()
+        case .music5:
+            R.string.localizable.musicCollectionItemJazz()
+        case .music6:
+            R.string.localizable.musicCollectionItemPop()
+        case .music7:
+            R.string.localizable.musicCollectionItemLightBeat()
+        }
+    }
+
+    var fileUrl: URL? {
+        switch self {
+        default:
+            return nil
+        }
+    }
+}
